3e9745488bbe21802e83839b43160144c2c13981
deniseherne <90498772+deniseherne@users.noreply.github.com>
2026-09-19 21:54:21 -0400

    Add cottage cheese baked ziti recipe

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'recipe-box-' + new URL(self.registration.scope).pathname + '-';
-const CACHE = PREFIX + '8701eb75faac';
+const CACHE = PREFIX + '2d5b2104d803';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './recipes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
