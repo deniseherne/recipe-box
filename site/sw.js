d0f9211a1bfd9dde3674bdb9685fa6b41bf0bfbe
deniseherne <90498772+deniseherne@users.noreply.github.com>
2026-09-19 21:49:53 -0400

    Add sheet pan chicken fajitas recipe

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'recipe-box-' + new URL(self.registration.scope).pathname + '-';
-const CACHE = PREFIX + 'e69d55e2f152';
+const CACHE = PREFIX + '24cb6e1a1b20';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './recipes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
