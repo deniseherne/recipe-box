bc44e94ed766ff71c6953c11c4447400d018c0d6
deniseherne <90498772+deniseherne@users.noreply.github.com>
2026-09-19 22:34:25 -0400

    Add keto blueberry smoothie recipe

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'recipe-box-' + new URL(self.registration.scope).pathname + '-';
-const CACHE = PREFIX + 'afc0da68e4fc';
+const CACHE = PREFIX + '06836c93ced5';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './recipes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
