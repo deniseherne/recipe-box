dafbb467ea370675666468a016eaa978e66e569c
deniseherne <90498772+deniseherne@users.noreply.github.com>
2026-09-19 23:03:19 -0400

    Add keto protein mousse recipe

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'recipe-box-' + new URL(self.registration.scope).pathname + '-';
-const CACHE = PREFIX + '12e04c183fa9';
+const CACHE = PREFIX + 'c1a473e62d38';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './recipes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
