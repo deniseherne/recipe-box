e0bc87db6d46ee2f7240e41dc63a6dd1fb12c943
deniseherne <90498772+deniseherne@users.noreply.github.com>
2026-09-19 23:13:22 -0400

    Add baked chicken parmesan recipe

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'recipe-box-' + new URL(self.registration.scope).pathname + '-';
-const CACHE = PREFIX + 'fc159a45926e';
+const CACHE = PREFIX + '09c5d48d7d43';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './recipes.js', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
